1e0be98b96afcef4c40d0396a3e84cb09b207f6d
Pablo Pinto <your-email@example.com>
2025-03-17 22:02:28 -0500

    Added missing Builder.io page

---
+++ src/app/pablos-new-new-home-page/page.tsx
@@ -0,0 +1,11 @@
+export default function CustomPage() {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">Welcome to the Builder.io Page</h1>
+        <p className="text-lg text-gray-600 mt-4">
+          This page is now connected to Builder.io!
+        </p>
+      </main>
+    );
+  }
+  
